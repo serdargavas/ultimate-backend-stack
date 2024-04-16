12c8e2cb8cd08b4806b464015e07e22e92500c38
serdargavas <serdargavas@gmail.com>
2024-04-16 23:13:31 +0300

    initial commit for repo starter kit and some upgrades within

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+const bootstrap = async () => {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(3000);
+};
+
+bootstrap();
